537efb56fbe291c8bbb8a307b396b9e77051bec2
Kenny Vo <28298773+vokenny@users.noreply.github.com>
2020-12-16 15:39:06 +0000

    Add Home snippet

--- js/utils.js
+++ js/utils.js
@@ -4,11 +4,20 @@
   var utils = {};
 
   var appTitle = "Bootstrap Practice";
+  
+  var homeHtmlUrl = "snippets/home.html";
 
   // Convenience function for inserting innerHTML for 'selector'
   function insertHtml (selector, html) {
-      var targetElem = document.querySelector(selector);
-      targetElem.innerHTML = html;
+    var targetElem = document.querySelector(selector);
+    targetElem.innerHTML = html;
+  };
+
+  // Show loading icon inside element identified by 'selector'
+  function showLoading (selector) {
+    var html = "<div class='text-center'>";
+    html += "<img id='ajax-loader' src='assets/images/ajax-loader.gif'></div>";
+    insertHtml(selector, html);
   };
 
   function updateTitle (heading) {
@@ -19,18 +28,30 @@
 
   utils.loadAndShowHome = function () {
     updateTitle("Home");
+    showLoading("#content");
+
+    $ajaxUtils.sendGetRequest(
+      homeHtmlUrl,
+      function (homeHtml) {
+        insertHtml("#content", homeHtml);
+      },
+      false
+    );
   };
 
   utils.loadAndShowTable = function () {
     updateTitle("Info Table");
+    showLoading("#content");
   };
 
   utils.loadAndShowGallery = function () {
     updateTitle("Photo Gallery");
+    showLoading("#content");
   };
 
   utils.loadAndShowCards = function () {
     updateTitle("Card Containers");
+    showLoading("#content");
   };
 
   global.$utils = utils;
