05178cf097b0bff483b13a7e87731db098b06814
Kenny Vo <28298773+vokenny@users.noreply.github.com>
2020-12-23 10:33:21 +0000

    Add gallery spec tests

---
+++ cypress/integration/gallery.spec.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+context("Gallery Spec", () => {
+  beforeEach(() => {
+    cy.visit("/index.html")
+    cy.get("#nav-gallery").click()
+    cy.get("title").should("contain", "Photo Gallery - Bootstrap Practice")
+    cy.get("h2").should("contain", "Photo Gallery")
+  })
+
+  describe("Gallery interaction", () => {
+    const thumbnails = require("../fixtures/gallery.json")
+
+    thumbnails.forEach((thumbnail) => {
+      it("Clicking each thumbnail should open the modal carousel, and be able to close it with 'Close' button", () => {
+        cy.get("#lightbox-modal").should("be.hidden")
+        cy.get(`#gallery > div:nth-child(${thumbnail.index}) > button`).click()
+
+        cy.get("#lightbox-modal").should("be.visible")
+        cy.get("#lightbox-modal-label").should("contain", "Gallery of Placeholder Images")
+        cy.get(".carousel-caption > h4").should("contain", thumbnail.caption)
+        cy.get("#close-modal").click()
+
+        cy.get("#lightbox-modal").should("be.hidden")
+        cy.get("h2")
+          .should("be.visible")
+          .should("contain", "Photo Gallery")
+      })
+    })
+  })
+
+})
